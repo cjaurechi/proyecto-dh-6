36c7ab9deb158a109a960a92a365a46c4f260a84
carlosjaurechi-asapp <50840438+carlosjaurechi-asapp@users.noreply.github.com>
2020-10-04 19:12:34 -0300

    Agrego estructura de página de carrito y css

--- sitio/app.js
+++ sitio/app.js
@@ -22,13 +22,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// Listen
+
+app.listen(3000, () => { console.log('Server corriendo'); });
+
+// Carrito de compras
+
+app.get('/carrito', function (req, res) {
+  let file = path.resolve('pages/productCart.html');
+  res.sendFile(file);
+});
+
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
