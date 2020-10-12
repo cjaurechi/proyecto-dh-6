29e2a3447b54e87efe3d59c68677e35fc811bed4
norberto-rubado <norberto_rubado@vfc.com.ar>
2020-10-12 18:35:04 -0300

    Desarrollo pagina detalle producto

--- sitio/app.js
+++ sitio/app.js
@@ -39,6 +39,13 @@ app.get('/carrito', function (req, res) {
   res.sendFile(file);
 });
 
+// Detalle de producto
+
+app.get('/producto', function (req, res) {
+  let file = path.resolve('pages/productDetail.html');
+  res.sendFile(file);
+});
+
 // Imagenes
 
 app.get('*', function (req, res) {
