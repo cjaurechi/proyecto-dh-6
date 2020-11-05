9ad778705de22f34170f32c41de790bcf0376cfa
norberto-rubado <norberto_rubado@vfc.com.ar>
2020-11-04 23:29:48 -0300

    Se generaron JSON de products , suppliers, productsImages, comments. Se genero app.css para los estilos de header.ejs y footer.ejs. Se modifico alta y modificacion productos segun nueva estructura de JSON

--- sitio/controllers/productsController.js
+++ sitio/controllers/productsController.js
@@ -1,11 +1,18 @@
 const fs = require('fs');
 const path = require('path');
+const { Console } = require('console');
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
+const productsImagesFilePath = path.join(__dirname, '../data/productsImagesDataBase.json');
+let products_images = JSON.parse(fs.readFileSync(productsImagesFilePath, 'utf-8'));
+
 const categoriesFilePath = path.join(__dirname, '../data/categoriesDataBase.json');
 const categories = JSON.parse(fs.readFileSync(categoriesFilePath, 'utf-8'));
+
+const suppliersFilePath = path.join(__dirname, '../data/suppliersDataBase.json');
+let suppliers = JSON.parse(fs.readFileSync(suppliersFilePath, 'utf-8'));
 
 const controller = {
 
@@ -21,57 +28,117 @@ const controller = {
 
     // Formulario de creacion
 	create: (req, res) => {
-		res.render("products/productCreateForm",{categories : categories});
+
+		suppliers = suppliers.filter(function(item) {
+			return (item.status == 'Habilitado')
+		})
+
+		res.render("products/productCreateForm",{categories : categories,suppliers : suppliers});
 	},
 	
 	// Alta de producto
 	store: (req, res, next) => {
 		
-		var product_image = ""
-		if (req.files[0] !== undefined) {
-			product_image = req.files[0].filename
+		for (let i = 0; i< req.files.length;i++) {
+			var product_image = ""
+			if (req.files[i] !== undefined) {
+				product_image = req.files[i].filename
+			}
+			products_images.push ({
+				id : products[products.length-1].id + 1,
+				image : product_image,
+				number : i,
+			})
 		}
 
-		console.log (req.files[0],product_image)
+		let archivo = JSON.stringify(products_images);
+		fs.writeFileSync (productsImagesFilePath,archivo);
+
+		fecha_actual = new Date()
 
 		products.push ({
 			id : products[products.length-1].id + 1,
-			image : product_image,
-			...req.body
+			name : req.body.name,
+			description : req.body.description,
+			category : req.body.category,
+			creation_user : req.body.user,
+			creation_date : fecha_actual.getDate() + "/" + (fecha_actual.getMonth() +1) + "/" + fecha_actual.getFullYear(),
+			supplier : req.body.supplier,
+			price : req.body.price,
+			discount : req.body.discount,
+			life_date_from : req.body.life_date_from,
+			life_date_to : req.body.life_date_to,
+			stock : req.body.stock,
+			state : req.body.status,
 		});
 
-		let archivo = JSON.stringify(products);
+		archivo = JSON.stringify(products);
 		fs.writeFileSync (productsFilePath,archivo);
-		res.redirect("/");
+		res.redirect("/productos/crear");
 	},
 
 	// Formulario de modificacion
 	edit: (req, res) => {
 
-		product = products.find(function(item){
+		let product = products.find(function(item){
 			return (req.params.id == item.id);
 		})
 
-		res.render("products/productEditForm",{product : product,categories : categories});
+		suppliers = suppliers.filter(function(item) {
+			return (item.status == 'Habilitado')
+		})
+
+		product_images = products_images.filter(function(item) {
+			return (req.params.id == item.id)
+		})
+
+		res.render("products/productEditForm",{product : product,categories : categories,suppliers : suppliers, product_images : product_images});
 	},
 
 	// Modificacion de producto
 	update: (req, res, next) => {
 
+		if (req.files[0] !== undefined) {
+			products_images = products_images.filter(function(item) {
+				return (item.id != req.params.id)
+			})
+
+			for (let i = 0; i< req.files.length;i++) {
+				var product_image = ""
+				if (req.files[i] !== undefined) {
+					product_image = req.files[i].filename
+				}
+				products_images.push ({
+					id : req.params.id,
+					image : product_image,
+					number : i,
+				})
+			}
+		}
+
+		let archivo = JSON.stringify(products_images);
+		fs.writeFileSync (productsImagesFilePath,archivo);
+
 		products.forEach (function(item) {
 			if (item.id == req.params.id) {
 				item.name = req.body.name;
+				description = req.body.description;
+				category = req.body.category;
 				item.price = req.body.price;
 				item.discount = req.body.discount;
 				item.category = req.body.category;
 				item.description = req.body.description;
-				if (req.files[0] !== undefined) {
-					item.image = req.files[0].filename;
-				}
+				supplier = req.body.supplier;
+				price = req.body.price;
+				discount = req.body.discount;
+				life_date_from = req.body.life_date_from;
+				life_date_to = req.body.life_date_to;
+				stock = req.body.stock;
+				state = req.body.status;
 			}
 		});
 
-		let archivo = JSON.stringify(products);
+		archivo = JSON.stringify(products);
 		fs.writeFileSync (productsFilePath,archivo);
 
 		res.redirect("/");
