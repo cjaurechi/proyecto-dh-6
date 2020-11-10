2aa297b3f269381987a180d5e544f6a38440c031
Paulaskub <poli.skubic@gmail.com>
2020-11-10 08:51:39 -0300

    Agrego la pagina de como regalar, cambie el home para que sea mas resonsive y el banner para que quede bien, cambie en la parte del login el registrate aca para que lleve a registro

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,15 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 const categoriesFilePath = path.join(__dirname, '../data/categoriesDataBase.json');
 const categories = JSON.parse(fs.readFileSync(categoriesFilePath, 'utf-8'));
+
+const productsImagesFilePath = path.join(__dirname, '../data/productsImagesDataBase.json');
+let products_images = JSON.parse(fs.readFileSync(productsImagesFilePath, 'utf-8'));
 
 const controller = {
 
   // Home Page
 
   home: (req, res) => {
-    res.render('index');
+    res.render('index', {categories:categories, products:products});
   },
 
   nosotros: (req, res) => {
@@ -18,6 +24,9 @@ const controller = {
 
   contacto: (req, res) => {
     res.render('contacto');
+  },
+  comoregalar: (req, res) => {
+    res.render('comoregalar');
   }
 
 }
--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,13 @@ const indexController = require('../controllers/indexController')
 /*** HOME PAGE ***/
 router.get('/', indexController.home);
 
-
 /*** NOSOTROS ***/
 router.get('/nosotros', indexController.nosotros);
 
 /*** CONTACTO ***/
 router.get('/contacto', indexController.contacto);
+
+/*** COMO REGALAR ***/
+router.get('/comoregalar', indexController.comoregalar);
 
 module.exports = router;
