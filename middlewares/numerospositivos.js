1161a7fd5e6072f0814e8bb2abd4ff4844d064f2
Paulaskub <poli.skubic@gmail.com>
2020-11-16 20:53:57 -0300

    Cambios que no funcionaron

--- middlewares/numerospositivos.js
+++ middlewares/numerospositivos.js
@@ -4,7 +4,7 @@ function numerosPositivos (req, res, next){
     if (req.body.expiration_days < 0 || req.body.price< 0 || req.body.discoun< 0 || req.body.stock< 0) {
 
               
-        return res.render('products/productEditForm');
+        return res.render('productos/crear');
 
     }else{
     next ()
