4279170e65d2494e2286ffcf4af8791df046716e
cjaurechi <cjaurechi@asapp.com>
2020-12-03 20:38:43 -0300

    Corrección de styling de middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -11,18 +11,9 @@ function authMiddleware(req, res, next) {
     res.locals.path = req.originalUrl;
 
     if (req.session.user != undefined) {
-
-        //Recorro el json de usuarios para obtener el rol del usuario
-        // for (let i = 0; i < users.length; i++) {
-        //     if (users[i].email == req.session.user) {
-        //         usuarioRol = users[i].rol;
-        //     }
-        // }
-
         if (req.session.user.rol == "") {
             req.session.user.rol = "user"
         }
-
         switch (arrayUrl[1]) {
             case 'usuarios':
                 if (arrayUrl[2] == "registro") {
@@ -30,32 +21,28 @@ function authMiddleware(req, res, next) {
                     return res.render('error');
                 }
             case 'productos':
-                if ((arrayUrl[2] == "crear" ||
-                    arrayUrl[3] == "editar" || arrayUrl[2] == "listado") &
-                    req.session.user.rol != "admin") {
+                if ((arrayUrl[2] == "crear" || 
+                    arrayUrl[3] == "editar" || 
+                    arrayUrl[2] == "listado") & req.session.user.rol != "admin") {
                     res.locals.error = { stack: "Para acceder a esta pagina el usuario debe ser Administrador" }
                     return res.render('error')
                 }
         }
-
         next();
-
     } else {
-
         switch (arrayUrl[1]) {
             case 'carrito':
                 res.locals.error = { stack: "Para acceder a esta pagina debe estar logueado" }
                 return res.render('error')
-
             case 'productos':
                 if (arrayUrl[2] == "crear" ||
                     arrayUrl[3] == "detalle" ||
-                    arrayUrl[3] == "editar" || arrayUrl[2] == "listado") {
+                    arrayUrl[3] == "editar" || 
+                    arrayUrl[2] == "listado") {
                     res.locals.error = { stack: "Para acceder a esta pagina debe estar logueado" }
                     return res.render('error')
                 }
         }
-
         next();
     }
 }
