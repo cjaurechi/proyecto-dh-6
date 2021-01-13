12f40d0d9605ea97b7bee7832f54bc487d047e98
Paulaskub <poli.skubic@gmail.com>
2021-01-13 18:31:57 -0300

    Agregando la validacion del forntend en el correo para que sea valido

--- public/javascript/validationregister.js
+++ public/javascript/validationregister.js
@@ -1,1 +1,16 @@
-console.log('Hola')
+// const { validator } = require("sequelize/types/lib/utils/validator-extras")
+
+
+let emailInput = document.querySelector('input[name = email]')
+
+emailInput.addEventListener('keyup', function(){
+    if (validator.isEmail(emailInput.value)){
+        emailInput.classList.remove('error')
+        emailInput.classList.add('success')
+    }else{
+        emailInput.classList.remove('success')
+        emailInput.classList.add('error')
+    }
+})
+
+console.log(emailInput)
