212bd48b1ec8e4b81edbb7b06460add9c5c077a7
Paulaskub <poli.skubic@gmail.com>
2021-01-14 09:53:24 -0300

    Agregando validacion del apellido en el registro (backend)

--- middlewares/registerMiddleware.js
+++ middlewares/registerMiddleware.js
@@ -1,7 +1,8 @@
 const { check, validationResult, body } = require("express-validator");
 
 let registerMiddleware = [
-    check('name').isLength({ min: 2 }).withMessage('El nombre debe contener al menos 2caracteres'),
+    check('name').isLength({ min: 2 }).withMessage('El nombre debe contener al menos 2 caracteres'),
+    check('last_name').isLength({ min: 2 }).withMessage('El apellido debe contener al menos 2 caracteres'),
     check('email').isEmail().withMessage('Por favor escribí una dirección de correo electrónico válida'),
     check('password').isLength({ min: 6 }).withMessage('La contraseña debe contener al menos 6 caracteres'),
     body('passwordrepeat').custom((value, { req, loc, path }) => {
