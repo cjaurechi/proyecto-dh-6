2573fad3bbe9265ba530396e94f688916922293f
Paulaskub <poli.skubic@gmail.com>
2021-01-15 19:07:07 -0300

    Validacion del front de la fecha

--- middlewares/registerMiddleware.js
+++ middlewares/registerMiddleware.js
@@ -4,6 +4,13 @@ let registerMiddleware = [
     check('name').isLength({ min: 2 }).withMessage('El nombre debe contener al menos 2 caracteres'),
     check('last_name').isLength({ min: 2 }).withMessage('El apellido debe contener al menos 2 caracteres'),
     check('email').isEmail().withMessage('Por favor escribí una dirección de correo electrónico válida'),
+    check('fecnac').custom((value, { req }) => {
+        if(new Date(value) <= new Date(req.body.fecnac)) {
+            return false;
+        } else {
+            return true;
+        }
+    }).withMessage('La fecha es incorrecta'),
     check('password').isLength({ min: 8 }).withMessage('La contraseña debe contener al menos 8 caracteres'),
     body('passwordrepeat').custom((value, { req, loc, path }) => {
         if (value !== req.body.password) {
