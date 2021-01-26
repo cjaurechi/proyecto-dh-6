bb5997303e74684ac83258a0e0f57da1569ae7a5
Paulaskub <poli.skubic@gmail.com>
2021-01-26 17:47:43 -0300

    Cambio en la parte de recomendados del carrito

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,8 +1,28 @@
+const db = require('../database/models');
+
+
 const controller = {
 
     // Carrito de compra
     productCart: (req, res) => {
-        res.render('cart/productCart');
+
+        products = db.products.findAll(
+            {
+            where: { status: "Habilitado" },
+            include: [{ association: "product_image" }]
+            }
+          )
+      
+          categories = db.categories.findAll()
+      
+              Promise.all([products,categories])
+                  .then(function ([products, categories]) {
+              res.render('cart/productCart', {categories:categories, products:products});
+                  })
+                  .catch(error => {
+                      res.render('error', { error: error });
+                  })
+       
     }
 
 };
