908b45a3573ef08a12aec3e623abb8766b43481c
Paulaskub <poli.skubic@gmail.com>
2021-02-04 20:09:13 -0300

    Modificando un campo en el detalle del usuario porque quedo mal

--- controllers/api/usersController.js
+++ controllers/api/usersController.js
@@ -42,7 +42,7 @@ module.exports = {
 
 			let newUsers = {
 				id: user.id,
-				first_name: user.id,
+				first_name: user.first_name,
 				last_name: user.last_name,
 				email: user.email,
 				image: "http://localhost:3000/images/users/" + user.image,
