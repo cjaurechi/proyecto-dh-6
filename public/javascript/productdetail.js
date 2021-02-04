49341758ee87bc24daecf56689c93e5996256e6a
norberto-rubado <norberto_rubado@vfc.com.ar>
2021-02-04 15:07:56 -0300

    Se agrego funcionalidad a la pagina de detalle de producto para incrementar y decrementar la cantidad a comprar

---
+++ public/javascript/productdetail.js
@@ -0,0 +1,25 @@
+window.addEventListener("load",function() {
+    
+    console.log("paso por aca!")
+//  Campos Inputs 
+    const cantidad_menos = document.getElementById('imagen_cantidad_menos');
+    const cantidad_mas = document.getElementById('imagen_cantidad_mas');
+    const cantidad = document.getElementById('quantity');
+
+    cantidad_menos.addEventListener("click",function() {
+        valorMenos = Number.parseInt(cantidad.value)
+        if (valorMenos > 1 ) {
+            valorMenos = valorMenos - 1
+            cantidad.value = valorMenos
+        }
+    })
+
+    cantidad_mas.addEventListener("click",function() {
+        valorMas = Number.parseInt(cantidad.value)
+        if (valorMas <= 1000 ) {
+            valorMas = valorMas + 1
+            cantidad.value = valorMas
+        }
+    })
+
+})
