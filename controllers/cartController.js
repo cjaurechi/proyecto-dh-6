214cc43396ab7fea28011e6c752fb8b726c6df5a
cjaurechi <cjaurechi@asapp.com>
2021-02-20 14:37:13 -0300

    Agrego actualización de productos en el carrito y saco tooltip de perfil en header

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -37,29 +37,72 @@ const controller = {
         const errors = validationResult(req);
 
         if (errors.isEmpty()) {
-            // Busco el producto que voy a agregar como Item
-            db.products.findByPk(req.body.product_id)
-                .then((product) => {
-                    // Saco el valor del producto, teniendo en cuenta el descuento.
-                    let price =
-                        Number(product.discount) > 0 ?
-                        product.price - (product.price * product.discount) / 100 :
-                        product.price;
+            db.items.findOne({
+                where: {
+                    user_id: req.session.user.id,
+                    product_id: req.body.product_id
+                }
+            }).then(response => {
+                if (response != undefined) {
+                    let order_quantity = response.quantity;
+                    db.items.destroy({
+                        where: {
+                            user_id: req.session.user.id,
+                            product_id: req.body.product_id
+                        }
+                    }).then(response => {
+                        db.products.findByPk(req.body.product_id)
+                            .then((product) => {
+                                // Saco el valor del producto, teniendo en cuenta el descuento.
+                                let price =
+                                    Math.ceil(Number(product.discount) > 0 ?
+                                    product.price - (product.price * product.discount) / 100 :
+                                    product.price);
 
-                    // Creo el Item de compra
-                    return db.items.create({
-                        sale_price: price,
-                        quantity: req.body.quantity,
-                        subtotal: price * req.body.quantity,
-                        state: 1,
-                        user_id: req.session.user.id,
-                        seller_id: product.supplier_id,
-                        product_id: product.id,
-                        created_at: moment(new Date()).format('YYYY-MM-DD')
-                    });
-                })
-                .then((item) => res.redirect('/carrito'))
-                .catch((err) => console.log(err.message));
+                                // Creo el Item de compra
+                                return db.items.create({
+                                    sale_price: price,
+                                    quantity: parseInt(req.body.quantity) + parseInt(order_quantity),
+                                    subtotal: price * (parseInt(req.body.quantity) + parseInt(order_quantity)),
+                                    state: 1,
+                                    user_id: req.session.user.id,
+                                    seller_id: product.supplier_id,
+                                    product_id: product.id,
+                                    created_at: moment(new Date()).format('YYYY-MM-DD')
+                                });
+                            })
+                            .then((item) => res.redirect('/carrito'))
+                            .catch((err) => console.log(err.message));
+                    })
+                } else {
+                    db.products.findByPk(req.body.product_id)
+                        .then((product) => {
+                            // Saco el valor del producto, teniendo en cuenta el descuento.
+                            let price =
+                                Number(product.discount) > 0 ?
+                                product.price - (product.price * product.discount) / 100 :
+                                product.price;
+
+                            // Creo el Item de compra
+                            return db.items.create({
+                                sale_price: price,
+                                quantity: req.body.quantity,
+                                subtotal: price * req.body.quantity,
+                                state: 1,
+                                user_id: req.session.user.id,
+                                seller_id: product.supplier_id,
+                                product_id: product.id,
+                                created_at: moment(new Date()).format('YYYY-MM-DD')
+                            });
+                        })
+                        .then((item) => res.redirect('/carrito'))
+                        .catch((err) => console.log(err.message));
+                }
+            })
+
+
+            // Busco el producto que voy a agregar como Item
+
         } else {
             db.products.findByPk(req.body.product_id)
                 .then(product => {
